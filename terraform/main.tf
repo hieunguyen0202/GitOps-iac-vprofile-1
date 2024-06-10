f8d3112bea5f7d6ab4e6b58bb7ebcef1c744389c
you <you@example.com>
2024-06-10 10:16:51 +0700

    bdxbdf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 
 # Define provider
 provider "google" {
-  credentials = file("C:\\Users\\HP\\Desktop\\actions\\GitOps-iac-vprofile-1\\terraform\\key.json")
+  credentials = file("C:/Users/HP/Desktop/actions/GitOps-iac-vprofile-1/terraform/key.json")
   project     = var.project_id
   region      = var.region
 }
