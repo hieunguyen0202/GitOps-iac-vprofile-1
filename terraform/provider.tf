02ed0e6387d80c80ebd313a593af4d104179dd1a
you <you@example.com>
2024-06-10 11:39:35 +0700

    fbsdfb

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-#   credentials = file(var.credentials_file_path)
+  credentials = file(var.credentials_file_path)
   project     = var.project_id
   region      = var.region
 }
