5d051f446733fd9757a52556600227f4c2f59f72
you <you@example.com>
2024-06-10 10:06:19 +0700

    sdfsdsd

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,7 @@
 
-
 # Define provider
 provider "google" {
-  credentials = "${file(var.credentials_file_path)}"
-  project = var.project_id
-  region  = var.region
+  credentials = file("${path.module}/${var.credentials_file_path}")
+  project     = var.project_id
+  region      = var.region
 }
-
-
-##
