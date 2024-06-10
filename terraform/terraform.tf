133992dc95c17ef801f082d8f872a709413e02ea
you <you@example.com>
2024-06-10 09:54:49 +0700

    434343434

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -22,12 +22,12 @@ terraform {
     }
   }
 
-  backend "gcs" {
-    bucket = "my-gitops-project-state"
-    prefix = "terraform/state"
-    # credentials = "${var.credentials_file_path}"
+  # backend "gcs" {
+  #   bucket = "my-gitops-project-state"
+  #   prefix = "terraform/state"
+  #   # credentials = "${var.credentials_file_path}"
 
-  }
+  # }
 
   required_version = ">= 1.6.0"
 }
